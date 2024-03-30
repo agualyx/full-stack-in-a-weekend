eaf9b577a2177c849b027816d9aa40370e8d5e59
agualyx <778565515@qq.com>
2024-03-31 02:18:40 +0800

    finish router

--- react-frontend-01-starting-setup/src/App.js
+++ react-frontend-01-starting-setup/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { NewPlaces } from './places/pages/NewPlaces';
+import { User } from './user/pages/User';
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<User/>} />
+        <Route path='/new/places' element={<NewPlaces/>} />
+        <Route path='*' element={<User/>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ react-frontend-01-starting-setup/src/places/pages/NewPlaces.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NewPlaces = () => {
+  return (
+    <h1>NewPlaces</h1>
+  )
+}
